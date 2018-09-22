b662cdca4e49ff14fe9cb40f23930be573917ddb
WitBash <yr-83p@mail.ru>
2018-09-22 10:27:24 +0300

    ready HomeWork1 task2

---
+++ HomeWork1/task2/ArrayConversion.java
@@ -0,0 +1,23 @@
+package HomeWork1.task2;
+
+import java.util.ArrayList;
+
+public class ArrayConversion <T> {
+    private T[] array;
+    private ArrayList<T> arrayList;
+
+    public ArrayConversion(T[] array) {
+        this.array = array;
+    }
+
+    public void arrayToArrayList(){
+        arrayList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            arrayList.add(array[i]);
+        }
+        for (T o: arrayList) {
+            System.out.print(o+" ");
+        }
+        System.out.println();
+    }
+}
